d065bfac69a2e43fe0c1f5200ace660b5b989f20
A-Cdeveloper <aleksandar@e-vlasotince.info>
2023-09-03 17:50:44 +0200

    lazy optimised

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import AppLayout from "./ui/AppLayout";
-import Dashboard from "./pages/Dashboard";
-import Bookings from "./pages/Bookings";
-import Cabins from "./pages/Cabins";
-import Users from "./pages/Users";
-import Settings from "./pages/Settings";
-import Account from "./pages/Account";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import Booking from "./pages/Booking";
-import CheckIn from "./pages/CheckIn";
-import ProtectedRoute from "./ui/ProtectedRoute";
+// import AppLayout from "./ui/AppLayout";
+// import Dashboard from "./pages/Dashboard";
+// import Bookings from "./pages/Bookings";
+// import Cabins from "./pages/Cabins";
+// import Users from "./pages/Users";
+// import Settings from "./pages/Settings";
+// import Account from "./pages/Account";
+// import Login from "./pages/Login";
+// import PageNotFound from "./pages/PageNotFound";
+// import Booking from "./pages/Booking";
+// import CheckIn from "./pages/CheckIn";
+// import ProtectedRoute from "./ui/ProtectedRoute";
+
+// LAZY LOADING COMPS /////////////////////////////////////
+const AppLayout = lazy(() => import("./ui/AppLayout"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Bookings = lazy(() => import("./pages/Bookings"));
+const Cabins = lazy(() => import("./pages/Cabins"));
+const Users = lazy(() => import("./pages/Users"));
+const Settings = lazy(() => import("./pages/Settings"));
+const Account = lazy(() => import("./pages/Account"));
+const Login = lazy(() => import("./pages/Login"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Booking = lazy(() => import("./pages/Booking"));
+const CheckIn = lazy(() => import("./pages/CheckIn"));
+const ProtectedRoute = lazy(() => import("./ui/ProtectedRoute"));
+const Spinner = lazy(() => import("./ui/Spinner"));
 
 import GlobalStyle from "./styles/GlobalStyles";
 import { DarkModeContextProvider } from "./context/DarkModeContext";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
@@ -36,28 +52,30 @@ const App = () => {
         <ReactQueryDevtools initialIsOpen={false} />
         <GlobalStyle />
         <BrowserRouter>
-          <Routes>
-            <Route
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate to="dashboard" replace />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="bookings/:bookingId" element={<Booking />} />
-              <Route path="checkin/:bookingId" element={<CheckIn />} />
-              <Route path="cabins" element={<Cabins />} />
-              <Route path="users" element={<Users />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="account" element={<Account />} />
-            </Route>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate to="dashboard" replace />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="bookings" element={<Bookings />} />
+                <Route path="bookings/:bookingId" element={<Booking />} />
+                <Route path="checkin/:bookingId" element={<CheckIn />} />
+                <Route path="cabins" element={<Cabins />} />
+                <Route path="users" element={<Users />} />
+                <Route path="settings" element={<Settings />} />
+                <Route path="account" element={<Account />} />
+              </Route>
 
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+              <Route path="login" element={<Login />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
         <Toaster
           position="bottom-left"
